a2cd606471351da8e9e77228d6d99a5fd12609d8
Heng-W <heng_wei@163.com>
2020-09-14 15:59:48 +0800

    up

--- compile.bat
+++ compile.bat
@@ -2,10 +2,14 @@
 setlocal enabledelayedexpansion
 set compile_cnt=0
 set build_cnt=0
-del build\windows\*.exe
+IF EXIST build (
+    del build\*.exe
+) else (
+    md build
+)
 for %%i in (*.cpp) do (
 	set source=%%i
-	set target=build\windows\!source:~0,-4!.exe
+	set target=build\!source:~0,-4!.exe
 	set /a compile_cnt+=1
 	echo No.!compile_cnt! compile !source!
 	g++ -std=c++11 -fexec-charset=gbk !source! -o !target!
